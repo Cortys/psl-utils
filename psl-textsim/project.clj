22a016c495993e667813bbaae9607e06e5ecca68
Clemens Damke <admin@cortys.de>
2017-10-05 13:37:35 +0200

    fixed autogenerated project.clj

--- psl-textsim/project.clj
+++ psl-textsim/project.clj
@@ -1,6 +1,6 @@
-(defproject org.linqs/psl-textsim "1.1.0-SNAPSHOT" 
+(defproject org.linqs/psl-textsim "1.1.0-SNAPSHOT"
   :description "Text similarity extensions to the PSL software from the LINQS research group."
-  :dependencies [[org.linqs/psl-core "${psl-core-version}"]
+  :dependencies [[org.linqs/psl-core "2.1.0-SNAPSHOT"]
                  [org.ujmp/ujmp-complete "0.2.4"]
                  [log4j "1.2.17"]
                  [org.slf4j/slf4j-api "1.7.21"]
@@ -12,4 +12,8 @@
                  [net.sourceforge.collections/collections-generic
                   "4.01"]
                  [com.wcohen/secondstring "20120620"]]
-  :parent [org.linqs/psl-utils "1.1.0-SNAPSHOT"])
+
+  :repositories {"psl-releases" "https://linqs-data.soe.ucsc.edu/maven/repositories/psl-releases"
+                 "psl-thirdparty" "https://linqs-data.soe.ucsc.edu/maven/repositories/psl-thirdparty"}
+
+  :java-source-paths ["src/main/java"])
